a3a0ce1df5e7776d6e6bcec58efc2a0c384c4147
g-Barsani <103264805+g-Barsani@users.noreply.github.com>
2023-08-24 22:50:50 -0300

    Add basic UI

--- scipt.js
+++ scipt.js
@@ -18,35 +18,63 @@ else
     return 2;
 }
 
-//console.log(`The game result is: ${playRound(playerSelection, computerSelection)}`);
 game();
 
 function game() {
-    const TOTAL_ROUNDS = 5;
-    for (let i = 0; i < TOTAL_ROUNDS; i++) {
-        let computerSelection = getComputerChoice();
-        let playerSelection = prompt().toLowerCase();
-        while (true) {
-            if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+    // const TOTAL_ROUNDS = 5;
+    // for (let i = 0; i < TOTAL_ROUNDS; i++) {
+    //     let computerSelection = getComputerChoice();
+    //     let playerSelection = prompt().toLowerCase();
+    //     while (true) {
+    //         if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+    //             break;
+    //         } else {
+    //             console.log('Please, enter a valid option!');
+    //             playerSelection = prompt().toLowerCase();
+    //         }
+    //     }
+        
+        
+    //     let gameResult = playRound(playerSelection, computerSelection);
+  
+    //     switch (gameResult) {
+    //       case 1: 
+    //         console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    //         break;
+    //       case 2:
+    //         console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    //         break;
+    //       default: 
+    //         console.log(`It's a tie! ${playerSelection} and ${computerSelection}`);
+    //     }
+    // }
+
+
+    let playerSelection = null;
+    const buttonBar = document.querySelector('.button-bar');
+
+    buttonBar.addEventListener('click', e => {
+        const button = e.target;
+
+        if (button.tagName === "BUTTON") {
+            const buttonId = e.target.id;
+            playerSelection = buttonId;
+            let computerSelection = getComputerChoice();
+            // let computerSelection = "paper";
+
+            let gameResult = playRound(playerSelection, computerSelection);
+            const results = document.getElementById('results'); 
+            
+            switch (gameResult) {
+                case 1: 
+                results.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
                 break;
-            } else {
-                console.log('Please, enter a valid option!');
-                playerSelection = prompt().toLowerCase();
+                case 2:
+                results.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+                break;
+                default: 
+                results.textContent = `It's a tie! ${playerSelection} and ${computerSelection}`;
             }
         }
-        
-        
-        let gameResult = playRound(playerSelection, computerSelection);
-  
-        switch (gameResult) {
-          case 1: 
-            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-            break;
-          case 2:
-            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-            break;
-          default: 
-            console.log(`It's a tie! ${playerSelection} and ${computerSelection}`);
-        }
-    }
+    })
   }
